c1afb2f29861ad0c8ff244576e2dcd881af143bf
Geordy <ggeordymm@gmail.com>
2023-11-15 20:36:24 -0500

    starting

---
+++ Formularios/FrmLogin.Designer.cs
@@ -0,0 +1,231 @@
+﻿
+namespace Sistema_Cobranza.Formularios
+{
+    partial class FrmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.TittleBar = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtClave = new System.Windows.Forms.TextBox();
+            this.TxtUsuario = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.BtnVerClave = new System.Windows.Forms.Button();
+            this.BtnCancel = new System.Windows.Forms.Button();
+            this.BtnLogin = new System.Windows.Forms.Button();
+            this.PbClose = new System.Windows.Forms.PictureBox();
+            this.TittleBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PbClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TittleBar
+            // 
+            this.TittleBar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.TittleBar.Controls.Add(this.label7);
+            this.TittleBar.Controls.Add(this.PbClose);
+            this.TittleBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.TittleBar.Location = new System.Drawing.Point(0, 0);
+            this.TittleBar.Name = "TittleBar";
+            this.TittleBar.Size = new System.Drawing.Size(293, 38);
+            this.TittleBar.TabIndex = 124;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(4, 6);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(248, 26);
+            this.label7.TabIndex = 125;
+            this.label7.Text = "    INGRESAR AL SISTEMA";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(110, 111);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 27);
+            this.label2.TabIndex = 133;
+            this.label2.Text = "CLAVE";
+            // 
+            // TxtClave
+            // 
+            this.TxtClave.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtClave.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtClave.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.TxtClave.Location = new System.Drawing.Point(34, 142);
+            this.TxtClave.MaxLength = 15;
+            this.TxtClave.Name = "TxtClave";
+            this.TxtClave.Size = new System.Drawing.Size(198, 35);
+            this.TxtClave.TabIndex = 132;
+            this.TxtClave.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.TxtClave.UseSystemPasswordChar = true;
+            // 
+            // TxtUsuario
+            // 
+            this.TxtUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.TxtUsuario.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtUsuario.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.TxtUsuario.Location = new System.Drawing.Point(34, 71);
+            this.TxtUsuario.MaxLength = 15;
+            this.TxtUsuario.Name = "TxtUsuario";
+            this.TxtUsuario.Size = new System.Drawing.Size(198, 35);
+            this.TxtUsuario.TabIndex = 131;
+            this.TxtUsuario.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label1.Location = new System.Drawing.Point(101, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 27);
+            this.label1.TabIndex = 130;
+            this.label1.Text = "USUARIO";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label3.Location = new System.Drawing.Point(24, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 23);
+            this.label3.TabIndex = 137;
+            this.label3.Text = "Sistema de Cobranza";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label4.Location = new System.Drawing.Point(218, 255);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 23);
+            this.label4.TabIndex = 138;
+            this.label4.Text = "2023";
+            // 
+            // BtnVerClave
+            // 
+            this.BtnVerClave.Image = ((System.Drawing.Image)(resources.GetObject("BtnVerClave.Image")));
+            this.BtnVerClave.Location = new System.Drawing.Point(241, 149);
+            this.BtnVerClave.Margin = new System.Windows.Forms.Padding(4);
+            this.BtnVerClave.Name = "BtnVerClave";
+            this.BtnVerClave.Size = new System.Drawing.Size(32, 28);
+            this.BtnVerClave.TabIndex = 136;
+            this.BtnVerClave.UseVisualStyleBackColor = true;
+            // 
+            // BtnCancel
+            // 
+            this.BtnCancel.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnCancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnCancel.Image = global::Sistema_Cobranza.Properties.Resources.cancelar;
+            this.BtnCancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnCancel.Location = new System.Drawing.Point(139, 186);
+            this.BtnCancel.Name = "BtnCancel";
+            this.BtnCancel.Size = new System.Drawing.Size(141, 65);
+            this.BtnCancel.TabIndex = 135;
+            this.BtnCancel.Text = "      Cancelar";
+            this.BtnCancel.UseVisualStyleBackColor = true;
+            // 
+            // BtnLogin
+            // 
+            this.BtnLogin.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.BtnLogin.Image = global::Sistema_Cobranza.Properties.Resources.llave;
+            this.BtnLogin.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.BtnLogin.Location = new System.Drawing.Point(10, 185);
+            this.BtnLogin.Name = "BtnLogin";
+            this.BtnLogin.Size = new System.Drawing.Size(120, 67);
+            this.BtnLogin.TabIndex = 134;
+            this.BtnLogin.Text = "        Login";
+            this.BtnLogin.UseVisualStyleBackColor = true;
+            // 
+            // PbClose
+            // 
+            this.PbClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.PbClose.Image = ((System.Drawing.Image)(resources.GetObject("PbClose.Image")));
+            this.PbClose.Location = new System.Drawing.Point(252, 6);
+            this.PbClose.Name = "PbClose";
+            this.PbClose.Size = new System.Drawing.Size(28, 28);
+            this.PbClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.PbClose.TabIndex = 124;
+            this.PbClose.TabStop = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 287);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.BtnVerClave);
+            this.Controls.Add(this.BtnCancel);
+            this.Controls.Add(this.BtnLogin);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtClave);
+            this.Controls.Add(this.TxtUsuario);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TittleBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.TittleBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PbClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel TittleBar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox PbClose;
+        private System.Windows.Forms.Button BtnVerClave;
+        private System.Windows.Forms.Button BtnCancel;
+        private System.Windows.Forms.Button BtnLogin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtClave;
+        private System.Windows.Forms.TextBox TxtUsuario;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
